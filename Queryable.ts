6933199cb3b5f87a430c89161e26709362d546a7
Sibren <0924655@hr.nl>
2019-04-06 15:25:21 +0200

    add PickKeys type

--- Queryable.ts
+++ Queryable.ts
@@ -51,3 +51,7 @@ function sortDesc<T, K extends keyof T>(data: Array<T>, property: K): Array<T> {
 }
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+type PickKeys<T, Conditions extends Array<keyof T>> = Pick<T, {
+    [K in keyof T]: K extends (Conditions extends Array<infer O> ? O : never) ? K : never
+}[keyof T]>
